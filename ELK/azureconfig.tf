881ac79d11cfcf4ae692824d7a060ba18c41643c
nvtuluva <nvtuluva@sysgain.com>
2017-09-26 22:30:21 +0530

    Create azureconfig.tf

---
+++ ELK/azureconfig.tf
@@ -0,0 +1,6 @@
+provider "azurerm" {
+  subscription_id = "086ef973-2199-477b-9b40-c3d05c01a287"
+  client_id       = "a382813c-e42b-44da-b84e-33acc27554e9"
+  client_secret   = "022fd881-d0fa-45f1-99eb-e32cbe428b0d"
+  tenant_id       = "dcf9e4d3-f44a-4c28-be12-8245c0d35668"
+}
