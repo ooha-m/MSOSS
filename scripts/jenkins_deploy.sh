14fe41ac48f7bd4ca55de9d68dca79959ad1d140
sushmithan <sushmithan@sysgain.com>
2017-09-27 18:45:23 +0530

    Update jenkins_deploy.sh

--- scripts/jenkins_deploy.sh
+++ scripts/jenkins_deploy.sh
@@ -4,10 +4,12 @@ user="admin"
 passwd=`cat /var/lib/jenkins/secrets/initialAdminPassword`
 url="localhost:8080"
 apt-get install html-xml-utils
-wget -P $srcdir $url/jnlpJars/jenkins-cli.jar
+wget -P $srcdir $srcdir/jnlpJars/jenkins-cli.jar
 java -jar $srcdir/jenkins-cli.jar -s http://$url who-am-i --username $user --password $passwd
-api=`curl --silent --basic http://'$user':'$passwd'@'$url'/user/admin/configure | hxselect '#apiToken' | sed 's/.*value="\([^"]*\)".*/\1\n/g'`
-CRUMB=`curl -s 'http://'$user':'$api'@'$url'/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)'`
-curl -X POST -d '<jenkins><install plugin="packer@current" /></jenkins>' --header 'Content-Type: text/xml' -H "$CRUMB" http://'$user':'$api'@'$url'/pluginManager/installNecessaryPlugins
-curl -X POST -d '<jenkins><install plugin="terraform@current" /></jenkins>' --header 'Content-Type: text/xml' -H "$CRUMB" http://'$user':'$api'@'$url'/pluginManager/installNecessaryPlugins
+api=`curl --silent --basic http://$user:$passwd@$url/user/admin/configure | hxselect '#apiToken' | sed 's/.*value="\([^"]*\)".*/\1\n/g'`
+CRUMB=`curl 'http://'$user':'$api'@'$url'/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)'`
+echo $api $CRUMB
+echo $CRUMB
+curl -X POST -d '<jenkins><install plugin="packer@current" /></jenkins>' --header 'Content-Type: text/xml' -H "$CRUMB" http://$user:$api@$url/pluginManager/installNecessaryPlugins
+curl -X POST -d '<jenkins><install plugin="terraform@current" /></jenkins>' --header 'Content-Type: text/xml' -H "$CRUMB" http://$user:$api@$url/pluginManager/installNecessaryPlugins
 systemctl restart jenkins
