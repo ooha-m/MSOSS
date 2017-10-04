c2b61b06e0bf14f289a6d6572821ec0912f5026b
udaykumartadka <udaykumart@sysgain.com>
2017-10-04 10:03:59 +0530

    Update jenkins_deploy.sh

--- scripts/jenkins_deploy.sh
+++ scripts/jenkins_deploy.sh
@@ -16,10 +16,35 @@ curl -X POST -d '<jenkins><install plugin="terraform@current" /></jenkins>' --he
 sleep 30 && java -jar $srcdir/jenkins-cli.jar -s  http://$url restart --username $user --password $passwd
 wget -P $srcdir https://raw.githubusercontent.com/sysgain/MSOSS/staging/scripts/elk-config.xml
 wget -P $srcdir https://raw.githubusercontent.com/sysgain/MSOSS/staging/scripts/packer-config.xml
+
+if [ ! -f "elk-config.xml" ]
+then
+	xmlstarlet ed -u '//buildWrappers/org.jenkinsci.plugins.terraform.TerraformBuildWrapper/variables' -v "subscription_id = &quot;$1&quot;
+	client_id = &quot;$2&quot;
+	client_secret = &quot;$3&quot;
+	tenant_id= &quot;$4&quot;
+	ResourceGroup= &quot;$5&quot;
+	Location= &quot;$6&quot;
+	vnetName= &quot;$7&quot;
+	DynamicIP= &quot;$8&quot;
+	subnetName= &quot;$9&quot;
+	storageAccType= &quot;${10}&quot;
+	vmSize= &quot;${11}&quot;
+	vmName= &quot;${12}&quot;
+	userName= &quot;${13}&quot;
+	password= &quot;${14}&quot;" @$srcdir/elk-config.xml | sed "s/&amp;//g" > @$srcdir/elk_config.xml
+fi
+
+if [ ! -f "packer-config.xml" ]
+then
+	xmlstarlet ed -u '//publishers/biz.neustar.jenkins.plugins.packer.PackerPublisher/params' -v "-var &apos;client_id=$2&apos; -var &apos;client_secret=$3&apos; -var &apos;resource_group=$5&apos; -var &apos;storage_account=${15}&apos; -var &apos;subscription_id=$1&apos; -var &apos;tenant_id=$4&apos;" @$srcdir/packer-config.xml | sed "s/amp;//g" >@$srcdir/packer_config.xml
+
+fi
+	
 wget -P $jenkinsdir https://raw.githubusercontent.com/sysgain/MSOSS/staging/scripts/biz.neustar.jenkins.plugins.packer.PackerPublisher.xml
 wget -P $jenkinsdir https://raw.githubusercontent.com/sysgain/MSOSS/staging/scripts/org.jenkinsci.plugins.terraform.TerraformBuildWrapper.xml
 sleep 30 && java -jar $srcdir/jenkins-cli.jar -s  http://$url restart --username $user --password $passwd && sleep 30
-curl -X POST "http://$user:$api@$url/createItem?name=ELKJob" --data-binary "@$srcdir/elk-config.xml" -H "$CRUMB" -H "Content-Type: text/xml"
-curl -X POST "http://$user:$api@$url/createItem?name=PackerBuildJob" --data-binary "@$srcdir/packer-config.xml" -H "$CRUMB" -H "Content-Type: text/xml"
-curl -X POST "http://$user:$api@$url/createItem?name=AppDeployJob" --data-binary "@$srcdir/elk-config.xml" -H "$CRUMB" -H "Content-Type: text/xml"
-curl -X POST "http://$user:$api@$url/createItem?name=VMSSJob" --data-binary "@$srcdir/elk-config.xml" -H "$CRUMB" -H "Content-Type: text/xml"
+curl -X POST "http://$user:$api@$url/createItem?name=ELKJob" --data-binary "@$srcdir/elk_config.xml" -H "$CRUMB" -H "Content-Type: text/xml"
+curl -X POST "http://$user:$api@$url/createItem?name=PackerBuildJob" --data-binary "@$srcdir/packer_config.xml" -H "$CRUMB" -H "Content-Type: text/xml"
+curl -X POST "http://$user:$api@$url/createItem?name=AppDeployJob" --data-binary "@$srcdir/elk_config.xml" -H "$CRUMB" -H "Content-Type: text/xml"
+curl -X POST "http://$user:$api@$url/createItem?name=VMSSJob" --data-binary "@$srcdir/elk_config.xml" -H "$CRUMB" -H "Content-Type: text/xml"
