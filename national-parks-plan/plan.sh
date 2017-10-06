f00c4a4c933c7e234a0d325c2588e79a3a519758
root <root@npweb2.3jq0pozpcjputifjfltkljebpe.dx.internal.cloudapp.net>
2017-10-06 11:17:39 +0000

    plan file updated

--- national-parks-plan/plan.sh
+++ national-parks-plan/plan.sh
@@ -1,17 +1,14 @@
 pkg_name=national-parks
-pkg_description="A sample JavaEE Web app deployed in the Tomcat8 package"
 pkg_origin=oss
-pkg_version=0.1.1
+pkg_description="A sample JavaEE Web app deployed in the Tomcat8 package"
+pkg_version="0.1.1"
 pkg_maintainer="oss <ossnoreply@sysgain.com>"
 pkg_license=('Apache-2.0')
-pkg_source=https://github.com/sysgain/MSOSS
+pkg_source="https://github.com/sysgain/national-parks"
 pkg_deps=(core/tomcat8 core/jdk8 core/mongo-tools)
 pkg_build_deps=(core/git core/maven)
 pkg_expose=(8080)
 pkg_svc_user="root"
-
-# Override do_download() to pull our source code from GitHub instead
-# of downloading a tarball from a URL.
 do_download()
 {
     build_line "do_download() =================================================="
@@ -24,7 +21,7 @@ do_download()
 
     mkdir ${pkg_dirname}
     cd ${pkg_dirname}
-    GIT_SSL_NO_VERIFY=true git clone --branch ${pkg_name} https://github.com/sysgain/MSOSS.git
+    GIT_SSL_NO_VERIFY=true git clone --branch master https://github.com/sysgain/national-parks.git
     return 0
 }
 
@@ -55,8 +52,7 @@ do_install()
 {
     build_line "do_install() =================================================="
 
-    # Our source files were copied over to the HAB_CACHE_SRC_PATH in do_build(),
-    # so now they need to be copied into the root directory of our package through
+    # Our source files were copied over to the HAB_CACHE_SRC_PATH in do_build(),    # so now they need to be copied into the root directory of our package through
     # the pkg_prefix variable. This is so that we have the source files available
     # in the package.
 
@@ -67,7 +63,6 @@ do_install()
     # Copy our seed data so that it can be loaded into Mongo using our init hook
     cp -v ${source_dir}/national-parks.json ${PREFIX}/
 }
-
 # We verify our own source code because we cloned from GitHub instead of
 # providing a SHA-SUM of a tarball
 do_verify()
@@ -75,3 +70,4 @@ do_verify()
     build_line "do_verify() ==================================================="
     return 0
 }
+
