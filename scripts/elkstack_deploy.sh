1643f5b0c0e40165cce116c4be08b5de3bbbb819
nvtuluva <nvtuluva@sysgain.com>
2017-10-07 00:26:15 +0530

    Update elkstack_deploy.sh

--- scripts/elkstack_deploy.sh
+++ scripts/elkstack_deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 sudo add-apt-repository -y ppa:webupd8team/java
+sudo apt-get update
 # Enable silent install
 echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
 echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
-sudo apt-get update
 sudo apt-get -y install oracle-java8-installer
 wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
 echo "deb http://packages.elastic.co/elasticsearch/2.x/debian stable main" | sudo tee -a /etc/apt/sources.list.d/elasticsearch-2.x.list
