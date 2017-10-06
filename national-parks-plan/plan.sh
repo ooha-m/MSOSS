b6835bff183910eed6b443b380ba7830ff4401be
root <root@npweb1.3jq0pozpcjputifjfltkljebpe.dx.internal.cloudapp.net>
2017-10-06 12:58:45 +0000

    updated new code

--- national-parks-plan/plan.sh
+++ national-parks-plan/plan.sh
@@ -1,14 +1,20 @@
 pkg_name=national-parks
-pkg_origin=oss
 pkg_description="A sample JavaEE Web app deployed in the Tomcat8 package"
-pkg_version="0.1.1"
+pkg_origin=oss
+pkg_version=0.1.1
 pkg_maintainer="oss <ossnoreply@sysgain.com>"
 pkg_license=('Apache-2.0')
-pkg_source="https://github.com/sysgain/national-parks"
+pkg_source=https://github.com/sysgain/national-parks
 pkg_deps=(core/tomcat8 core/jdk8 core/mongo-tools)
 pkg_build_deps=(core/git core/maven)
 pkg_expose=(8080)
 pkg_svc_user="root"
+pkg_binds=(
+  [database]="port"
+)
+
+# Override do_download() to pull our source code from GitHub instead
+# of downloading a tarball from a URL.
 do_download()
 {
     build_line "do_download() =================================================="
@@ -52,7 +58,8 @@ do_install()
 {
     build_line "do_install() =================================================="
 
-    # Our source files were copied over to the HAB_CACHE_SRC_PATH in do_build(),    # so now they need to be copied into the root directory of our package through
+    # Our source files were copied over to the HAB_CACHE_SRC_PATH in do_build(),
+    # so now they need to be copied into the root directory of our package through
     # the pkg_prefix variable. This is so that we have the source files available
     # in the package.
 
@@ -63,6 +70,7 @@ do_install()
     # Copy our seed data so that it can be loaded into Mongo using our init hook
     cp -v ${source_dir}/national-parks.json ${PREFIX}/
 }
+
 # We verify our own source code because we cloned from GitHub instead of
 # providing a SHA-SUM of a tarball
 do_verify()
@@ -70,4 +78,3 @@ do_verify()
     build_line "do_verify() ==================================================="
     return 0
 }
-
