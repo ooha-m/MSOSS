63527ffb6098a8599a9d94063da17b04c14cc45d
unknown <srekapalli@sysgain.com>
2017-10-09 20:12:00 +0530

    updated

--- scripts/build.sh
+++ scripts/build.sh
@@ -18,8 +18,8 @@ sudo apt-get -y install apt-transport-https azure-cli
 mkdir /scripts
 echo "#!/bin/sh" >> /scripts/uploadhart.sh
 echo "HARTFILE=\$1" >> /scripts/uploadhart.sh
-echo "storageAccount='$storageAcc'"
-echo "export AZURE_STORAGE_ACCOUNT=$storageAccount"
+echo "storageAccount='$storageAcc'" >> /scripts/uploadhart.sh
+echo "export AZURE_STORAGE_ACCOUNT=$storageAccount" >> /scripts/uploadhart.sh
 echo "az login --service-principal -u '$appID' --password '$password' --tenant '$tenantID' > /dev/null" >> /scripts/uploadhart.sh
 echo "az storage container create --name AppHart --output table > /dev/null" >> /scripts/uploadhart.sh
 echo "az storage blob upload --container-name AppHart -f \$HARTFILE -n \$HARTFILE > /dev/null" >> /scripts/uploadhart.sh
