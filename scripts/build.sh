519732a7b4811313c6afa654ffae1cd38e8291e4
SrikalaRekapalli <srekapalli@sysgain.com>
2017-10-09 21:55:29 +0530

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -19,7 +19,8 @@ mkdir /scripts
 echo "#!/bin/sh" >> /scripts/uploadhart.sh
 echo "HARTFILE=\$1" >> /scripts/uploadhart.sh
 echo "storageAccount='$storageAcc'" >> /scripts/uploadhart.sh
-echo "export AZURE_STORAGE_ACCOUNT='$storageAcc'" >> /scripts/uploadhart.sh
+echo "export AZURE_STORAGE_ACCOUNT='$storageAccount'" >> /scripts/uploadhart.sh
 echo "az login --service-principal -u '$appID' --password '$password' --tenant '$tenantID' > /dev/null" >> /scripts/uploadhart.sh
-echo "az storage container create --name AppHart --output table > /dev/null" >> /scripts/uploadhart.sh
-echo "az storage blob upload --container-name AppHart -f \$HARTFILE -n \$HARTFILE > /dev/null" >> /scripts/uploadhart.sh
+echo "az storage container create --name apphart --output table > /dev/null" >> /scripts/uploadhart.sh
+echo "az storage blob upload --container-name apphart -f \$HARTFILE -n \$HARTFILE > /dev/null" >> /scripts/uploadhart.sh
+chmod +x /scripts/uploadhart.sh
