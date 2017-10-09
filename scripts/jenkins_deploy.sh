90cf460e98b8a93739697559705fcee3b7f53d2e
udaykumartadka <udaykumart@sysgain.com>
2017-10-09 22:15:41 +0530

    Update jenkins_deploy.sh

--- scripts/jenkins_deploy.sh
+++ scripts/jenkins_deploy.sh
@@ -102,7 +102,8 @@ vmName = &quot;${12}&quot;
 userName = &quot;${13}&quot;
 password = &quot;${14}&quot;
 sharedStorage = &quot;${15}&quot;
-imageUri = &quot;${16}&quot;" $srcdir/AppTerraformjob.xml | sed "s/&amp;quot;/\"/g" > $srcdir/AppTerraformjob.xml-newconfig.xml
+Hartfile = &quot;UpdateHartFile&quot;
+imageUri = &quot;UpdateUrl&quot;" $srcdir/AppTerraformjob.xml | sed "s/&amp;quot;/\"/g" > $srcdir/AppTerraformjob.xml-newconfig.xml
 fi
 
 if [ ! -f "MongoDBPackerjob.xml" ]
