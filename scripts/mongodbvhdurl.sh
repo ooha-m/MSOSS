8bc881118e819668f7c163e37d3762c142ea018b
udaykumartadka <udaykumart@sysgain.com>
2017-10-09 15:15:40 +0530

    Update mongodbvhdurl.sh

--- scripts/mongodbvhdurl.sh
+++ scripts/mongodbvhdurl.sh
@@ -5,5 +5,5 @@ tentantid=$3
 storageAcc=$4
 az login --service-principal -u $appid --password $clientsecret --tenant $tentantid
 az storage blob list -c system --account-name $storageAcc > vhd
-vhdurl=`cat vhd | jq '.[] | .name' | head -n 1 | awk -F/ '{print $4}' | sed 's/"//'`
+vhdurl=`cat vhd | jq '.[] | .name' | head -n 1 | sed 's/"//' | sed 's/"//'`
 echo $vhdurl
