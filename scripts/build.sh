c65ab9b385fc71f1ed7a22fc114b9839b3bf8d26
SrikalaRekapalli <srekapalli@sysgain.com>
2017-10-09 17:29:56 +0530

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -18,4 +18,5 @@ sudo apt-get -y install apt-transport-https azure-cli
 mkdir /scripts
 echo "#!/bin/sh" >> /scripts/uploadhart.sh
 echo "HARTFILE=\$1" >> /scripts/uploadhart.sh
+echo "storageAccount='$storageAcc'"
 echo "az login --service-principal -u '$appID' --password '$password' --tenant '$tenantID' > /dev/null" >> /scripts/uploadhart.sh
