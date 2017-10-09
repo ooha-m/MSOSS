0199d913251eda3395a3ce5298ee90635bbe6d80
SrikalaRekapalli <srekapalli@sysgain.com>
2017-10-09 16:40:35 +0530

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+appID = $1
+password = $2
+tenantID = $3
+storageAcc=$4
 sudo apt-get update
 sudo apt-get install openjdk-8-jdk -y
 addgroup hab
@@ -11,3 +15,7 @@ echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ wheezy ma
 sudo apt-key adv --keyserver packages.microsoft.com --recv-keys 417A0893 
 sudo apt-get update
 sudo apt-get -y install apt-transport-https azure-cli
+mkdir /scripts
+echo "#!/bin/sh" >> /scripts/hart.sh
+echo "HARTFILE='$1'" >> /scripts/hart.sh
+echo "az login --service-principal -u $appID --password $password --tenant $tenantID" >> /scripts/hart.sh
