c201d52bb34b7107bf140aa4fab371a2a116d7fc
udaykumartadka <udaykumart@sysgain.com>
2017-10-09 17:34:45 +0530

    Update mongodbvhdurl.sh

--- scripts/mongodbvhdurl.sh
+++ scripts/mongodbvhdurl.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-#srcdir="/usr/share/jenkins"
+srcdir="/usr/share/jenkins"
 #cat $srcdir/mongodbvhdurl.secrets
-appid=`awk -F, '{print $1}'`
-clientsecret=`awk -F, '{print $2}'`
-tentantid=`awk -F, '{print $3}'`
-storageAcc=`awk -F, '{print $4}'`
+appid=`awk -F, '{print $1}' $srcdir/mongodbvhdurl.secrets`
+clientsecret=`awk -F, '{print $2}' $srcdir/mongodbvhdurl.secrets`
+tentantid=`awk -F, '{print $3}' $srcdir/mongodbvhdurl.secrets`
+storageAcc=`awk -F, '{print $4}' $srcdir/mongodbvhdurl.secrets`
 az login --service-principal -u $appid --password $clientsecret --tenant $tentantid > /dev/null
 az storage blob list -c system --account-name $storageAcc > vhd
 vhdurl=`cat vhd | jq '.[] | .name' | sed 's/"//' | sed 's/"//' | grep *.vhd`
