1d0fc83db93602b433daf814091c37929a1956ec
SrikalaRekapalli <srekapalli@sysgain.com>
2017-10-09 15:56:27 +0530

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,3 +4,5 @@ sudo apt-get install openjdk-8-jdk -y
 addgroup hab
 sudo useradd -g hab hab
 usermod -aG sudo hab
+sleep 30
+curl https://raw.githubusercontent.com/habitat-sh/habitat/master/components/hab/install.sh | sudo bash
