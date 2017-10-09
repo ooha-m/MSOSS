d663baa64b0c11540f748da096bff260de199cd6
udaykumartadka <udaykumart@sysgain.com>
2017-10-09 16:04:25 +0530

    Update mongodbvhdurl.sh

--- scripts/mongodbvhdurl.sh
+++ scripts/mongodbvhdurl.sh
@@ -8,4 +8,5 @@ storageAcc=$4
 az login --service-principal -u $appid --password $clientsecret --tenant $tentantid
 az storage blob list -c system --account-name $storageAcc > vhd
 vhdurl=`cat vhd | jq '.[] | .name' | sed 's/"//' | sed 's/"//'`
-echo $vhdurl
+newvhdurl="https://$4.blob.core.windows.net/system/$vhdurl"
+echo $newvhdurl
