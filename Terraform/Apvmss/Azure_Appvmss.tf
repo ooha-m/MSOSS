24d679886453c01d0b91985cef71242ee22fa06d
nvtuluva <nvtuluva@sysgain.com>
2017-10-11 15:35:44 +0530

    Update Azure_Appvmss.tf

--- Terraform/Apvmss/Azure_Appvmss.tf
+++ Terraform/Apvmss/Azure_Appvmss.tf
@@ -122,7 +122,7 @@ output "Vm_Scale_Set_fqdn" {
     value = "${azurerm_public_ip.vmsspublicip.domain_name_label}.${var.Location}.cloudapp.azure.com}"
 }
 output "Application_URL" {
-    value = "${azurerm_public_ip.vmsspublicip.domain_name_label}.${var.Location}.cloudapp.azure.com}:8080/national_parks"
+    value = "${azurerm_public_ip.vmsspublicip.domain_name_label}.${var.Location}.cloudapp.azure.com}:8080/national-parks"
 }
 output "SSH_access_to_instace1" {
     value = "${azurerm_public_ip.vmsspublicip.domain_name_label}.${var.Location}.cloudapp.azure.com}:5000"
