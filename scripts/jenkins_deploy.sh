b478bf2c9f09e2e0e3cc9fa94d6958b85c8df844
udaykumartadka <udaykumart@sysgain.com>
2017-10-13 22:12:39 +0530

    Update jenkins_deploy.sh

--- scripts/jenkins_deploy.sh
+++ scripts/jenkins_deploy.sh
@@ -89,7 +89,9 @@ fi
 
 if [ ! -f "kubernetes.xml" ]
 then
-    xmlstarlet ed -u '//builders/hudson.tasks.Shell/command' -v "az login -u $FTP_USER -p $FTP_PASSWORD
+    username="\$FTP_USER"
+    password="\$FTP_PASSWORD"
+    xmlstarlet ed -u '//builders/hudson.tasks.Shell/command' -v "az login -u $username -p $password
 az account set --subscription $1
 az acs create --orchestrator-type kubernetes --name ${18} --resource-group $5 --admin-username ${13} --agent-count ${19} --agent-vm-size ${22} --dns-prefix ${17} --master-count ${21} --master-vm-size ${22} --generate-ssh-keys" $srcdir/kubernetes.xml | sed "s/&amp;quot;/\"/g" > $srcdir/kubernetes-newconfig.xml
 fi
