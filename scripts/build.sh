7743eaceb5b83c0b8dbd5bf509f8d0a18a15fb98
udaykumartadka <udaykumart@sysgain.com>
2017-10-20 17:07:46 +0530

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,7 @@ appID=$1
 password=$2
 tenantID=$3
 storageAcc=$4
+subscriptionID=$5
 echo "---Configure Repos for Azure Cli 2.0---"
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ wheezy main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
 sudo apt-key adv --keyserver packages.microsoft.com --recv-keys 417A0893
@@ -20,5 +21,6 @@ echo "storageAccount='$storageAcc'" >> /scripts/uploadhart.sh
 echo "export AZURE_STORAGE_ACCOUNT='$storageAcc'" >> /scripts/uploadhart.sh
 echo "az login --service-principal -u '$appID' --password '$password' --tenant '$tenantID' > /dev/null" >> /scripts/uploadhart.sh
 echo "az storage container create --name apphart --output table > /dev/null" >> /scripts/uploadhart.sh
+echo "az account set --subscription $subscriptionID" >> /scripts/uploadhart.sh
 echo "az storage blob upload --container-name apphart -f \$HARTFILE -n \$HARTFILE > /dev/null" >> /scripts/uploadhart.sh
 chmod +x /scripts/uploadhart.sh
