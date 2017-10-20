c7fb33b9f9318f30cec1c53ce2c1224828aeb04c
udaykumartadka <udaykumart@sysgain.com>
2017-10-21 00:36:26 +0530

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -20,7 +20,7 @@ echo "HARTFILE=\$1" >> /scripts/uploadhart.sh
 echo "storageAccount='$storageAcc'" >> /scripts/uploadhart.sh
 echo "export AZURE_STORAGE_ACCOUNT='$storageAcc'" >> /scripts/uploadhart.sh
 echo "az login --service-principal -u '$appID' --password '$password' --tenant '$tenantID' > /dev/null" >> /scripts/uploadhart.sh
-echo "az storage container create --name apphart --output table > /dev/null" >> /scripts/uploadhart.sh
 echo "az account set --subscription $subscriptionID" >> /scripts/uploadhart.sh
+echo "az storage container create --name apphart --output table > /dev/null" >> /scripts/uploadhart.sh
 echo "az storage blob upload --container-name apphart -f \$HARTFILE -n \$HARTFILE > /dev/null" >> /scripts/uploadhart.sh
 chmod +x /scripts/uploadhart.sh
